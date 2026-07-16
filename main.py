bb8360b070d3796e6900c13cd04d2d06c67531ad
rid47 <ridwanmizan@gmail.com>
2026-07-16 07:17:07 +0600

    done with openai api integration course

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 from dotenv import load_dotenv
 from openai import OpenAI
+from pydantic import BaseModel
 load_dotenv()
 
+
+class CodeOutput(BaseModel):
+    function_name: str
+    code: str
+    explanation: str
+    example_usage: str
+
+user_input = input("Describe the python function you want help with: ")
 
 
 client = OpenAI()
 
-code_response = client.responses.create(
+code_response = client.responses.parse(
     model="gpt-5",
-    input="Please write a simple Python function to add 2 numbers"
+    input=[
+        {
+            "role": "developer",
+            "content": (
+                "You are a Python coding assitant."
+                "Only accept Python-related questions."
+            ),
+        },
+        {
+            "role": "user",
+            "content": f"{user_input}"
+        }
+    ],
+
+    text_format=CodeOutput,
 )
 
 
-print(f"Function:\n{code_response.output_text}")
+code_result: CodeOutput = code_response.output_parsed
 
 
+print(f"Function: {code_result.function_name}")
+print(f"Code:\n{code_result.code}")
+print(f"Explanation: {code_result.explanation}")
+print(f"Example Usage: {code_result.example_usage}")
